cff7c2f4ad1fad1a4b7b0af1e4ecc8b7ea0dc068
paulo <paulorscontato@gmail.com>
2023-03-10 15:49:54 -0300

    teste de correção

--- js/script.js
+++ js/script.js
@@ -14,21 +14,20 @@ const fetchPokemon = async (pokemon) => {
     `https://pokeapi.co/api/v2/pokemon/${pokemon}`
   );
 
-  if (APIResponse.status ===200){
-      const data = await APIResponse.json();
-      return data;
-}
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    return data;
+  }
 };
 
 const renderPokemon = async (pokemon) => {
-
-    pokemonName.innerHTML = 'Loading ...';
-    pokemonNumber.innerHTML = '';
+  pokemonName.innerHTML = "Loading ...";
+  pokemonNumber.innerHTML = "";
 
   const data = await fetchPokemon(pokemon);
 
-if (data){
-    pokemonImage.style.display = 'block';
+  if (data) {
+    pokemonImage.style.display = "block";
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
     pokemonImage.src =
@@ -37,32 +36,29 @@ if (data){
       ];
     input.value = "";
     searchPokemon = data.id;
-} else {
-    pokemonImage.style.display = 'none';
-    pokemonName.innerHTML = 'Not Found';
-    pokemonNumber.innerHTML = '';
-    input.value = '';
-}
-
+  } else {
+    pokemonImage.style.display = "none";
+    pokemonName.innerHTML = "Not Found";
+    pokemonNumber.innerHTML = "";
+    input.value = "";
+  }
 };
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
   renderPokemon(input.value.toLowerCase());
 });
 
-buttonPrev.addEventListener('click', () => {
-    if (searchPokemon > 1){
-        searchPokemon -= 1;
-        renderPokemon(searchPokemon)
+buttonPrev.addEventListener("click", () => {
+  if (searchPokemon > 1) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+  }
+});
 
-    }
-})
+buttonNext.addEventListener("click", () => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon);
+});
 
-buttonNext.addEventListener('click', () => {
-    searchPokemon += 1;
-    renderPokemon(searchPokemon)
-})
-
-
-renderPokemon('1')
+renderPokemon(searchPokemon);
